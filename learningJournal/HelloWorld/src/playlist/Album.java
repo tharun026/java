b0386cb7bf908c29ae9c44bc9ad4d8b9155c5373
Sekar <tsekar2@LAMU02YQ2S5LVCJ.uhc.com>
2021-07-26 22:38:28 +0530

    26072021

---
+++ learningJournal/HelloWorld/src/playlist/Album.java
@@ -0,0 +1,52 @@
+package playlist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    public Song findSong(String title) {
+        for (Song checkedSong: songs) {
+            if (checkedSong.getTitle() == title) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if ( (index >= 0) && (index <= songs.size())) {
+            playList.add(songs.get(index));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        return false;
+    }
+}
